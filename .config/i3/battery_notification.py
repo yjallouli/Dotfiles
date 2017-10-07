41c1be51585d4fd320ef1cac809637f288550b5c
yjallouli <yjallouli@gmail.com>
2017-10-07 14:14:29 +0200

    Update battery_notification.py

--- .config/i3/battery_notification.py
+++ .config/i3/battery_notification.py
@@ -2,12 +2,17 @@ import subprocess
 import os
 from time import sleep
 
-while True :
-    battery_information = subprocess.check_output("acpi -b", shell=True)
-    battery_status = str(battery_information).split(", ")[0].split(": ")[1]
-    battery_percentage = int(str(battery_information).split(", ")[1].strip('%'))
+processesList = str(subprocess.check_output("ps aux | grep battery_notification", shell=True))
 
-    if battery_percentage <= 10 and battery_status == 'Discharging':
-        os.system('notify-send --icon=gtk-info "L O W   B A T T E R Y" "      Plug your cable" -t 10000')
+if processesList.count('battery_notification.py')<=1 :  # Launch only if the process is not already running
+	while True :
+	    battery_information = subprocess.check_output("acpi -b", shell=True)
+	    battery_status = str(battery_information).split(", ")[0].split(": ")[1]
+	    battery_percentage = int(str(battery_information).split(", ")[1].strip('%'))
 
-    sleep(10)
+	    if battery_percentage <= 10 and battery_status == 'Discharging':
+	        os.system('notify-send --icon=gtk-info "L O W   B A T T E R Y" "      Plug your cable" -t 10000')
+	    
+	    sleep(10)
+
+else : 	print('Process already running')
