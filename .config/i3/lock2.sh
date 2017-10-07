5d02ca1da7cc63be9151661b0dd80b1fbe996bb8
yjallouli <yjallouli@gmail.com>
2017-10-07 10:13:28 +0200

    Create lock2.sh

---
+++ .config/i3/lock2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+i3lock --insidevercolor=ffffff1c --insidewrongcolor=ffffff1c --insidecolor=0000001c --ringvercolor=ffffff00 --ringwrongcolor=ffffff55 --ringcolor=0000003e --textcolor=00000000 --linecolor=00000000 --keyhlcolor=B58800ff --bshlcolor=4AB7F4ff -i ~/.config/i3/desktop_locked.png --no-composite
